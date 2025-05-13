9fed074bbb4a4d0dcb54b9ce6dff61eb3b577b91
salamahin.a <salamahin.a@DSK-0704>
2025-05-13 16:57:29 +0300

    добавлен handler api/shorten

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	logger "github.com/AlexeySalamakhin/URLShortener/internal/logger"
+	"github.com/AlexeySalamakhin/URLShortener/internal/models"
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 type URLShortener interface {
@@ -25,7 +29,8 @@ func NewURLHandler(shortener URLShortener, baseURL string) *URLHandler {
 func (h *URLHandler) SetupRouter() *chi.Mux {
 	rout := chi.NewRouter()
 	rout.Use(logger.RequestLogger)
-	rout.Post("/", h.PostURLHandler)
+	rout.Post("/", h.PostURLHandlerText)
+	rout.Post("/api/shorten", h.PostURLHandlerJson)
 	rout.Get("/{shortURL}", h.GetURLHandler)
 	rout.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
@@ -33,7 +38,7 @@ func (h *URLHandler) SetupRouter() *chi.Mux {
 	return rout
 }
 
-func (h *URLHandler) PostURLHandler(w http.ResponseWriter, r *http.Request) {
+func (h *URLHandler) PostURLHandlerText(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	originalURL, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -47,6 +52,41 @@ func (h *URLHandler) PostURLHandler(w http.ResponseWriter, r *http.Request) {
 	shortKey := h.Shortener.Shorten(string(originalURL))
 	w.WriteHeader(201)
 	w.Write(fmt.Appendf(nil, "%s/%s", h.BaseURL, shortKey))
+}
+
+func (h *URLHandler) PostURLHandlerJson(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var req models.ShortenRequest
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		logger.Log.Error("Failed to decode JSON request", zap.Error(err))
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	if _, err := url.ParseRequestURI(req.URL); err != nil {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := h.Shortener.Shorten(string(req.URL))
+
+	resp := models.ShortenResponse{Result: fmt.Sprintf("%s/%s", h.BaseURL, shortKey)}
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		logger.Log.Error("Failed to encode JSON request", zap.Error(err))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	w.Write(jsonResp)
 }
 
 func (h *URLHandler) GetURLHandler(w http.ResponseWriter, r *http.Request) {
